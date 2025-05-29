3a06f344298ce778159e48962a86068e609e5c67
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-05-29 12:39:51 -0700

    implemented findExplorer method tests

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -1,4 +1,8 @@
 import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.util.Random;
+
 import org.junit.Test;
 
 public class ExplorerSearchTest {
@@ -17,4 +21,85 @@ public class ExplorerSearchTest {
 
     // Add more tests here!
     // Come up with varied cases
+
+ 
+    @Test
+    public void findExplorerFirstRowFirstColumn() {
+        int[][] island = {
+            {0,3,2,3,3},
+            {3,2,3,2,3},
+            {2,3,4,3,3},
+            {3,2,2,2,3},
+            {2,3,2,2,3},
+        };
+        int[] actual = ExplorerSearch.findExplorer(island);
+        int[] expected = {0, 0};
+        assertArrayEquals(expected, actual);
+    }
+
+     @Test
+    public void findExplorerMiddleRowMiddleColumn() {
+        int[][] island = {
+            {2,3,2,3,3},
+            {3,2,3,2,3},
+            {2,3,0,3,3},
+            {3,2,2,2,3},
+            {2,3,2,2,3},
+        };
+        int[] actual = ExplorerSearch.findExplorer(island);
+        int[] expected = {2, 2};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findExplorerLastRowLastColumn() {
+        int[][] island = {
+            {2,3,2,3,3},
+            {3,2,3,2,3},
+            {2,3,1,3,3},
+            {3,2,2,2,3},
+            {2,3,2,2,0},
+        };
+        int[] actual = ExplorerSearch.findExplorer(island);
+        int[] expected = {4, 4};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findExplorerRandomLocation() {
+        int[][] island = {
+            {2,2,2,2,2},
+            {2,2,2,2,2},
+            {2,2,2,2,2},
+            {2,2,2,2,2},
+            {2,2,2,2,2},
+        };
+        Random random = new Random();
+        int row = random.nextInt(island.length);
+        int col = random.nextInt(island[0].length);
+        island[row][col] = 0;
+
+        int[] actual = ExplorerSearch.findExplorer(island);
+        int[] expected = {row, col};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findExplorerNoExplorerFound() {
+        int[][] island = {
+            {2,3,2,3,3},
+            {3,2,3,2,3},
+            {2,3,4,3,3},
+            {3,2,2,2,3},
+            {2,3,2,2,3},
+        };
+         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ExplorerSearch.findExplorer(island);
+        });
+        assertEquals("No explorer found", exception.getMessage()); 
+    }
+
 }
+    
+
+   
